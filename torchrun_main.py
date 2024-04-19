8ea131e46b9cfe9bb808fa4fb402c2c07931d74a
Nammu <namratashivagunde@gmail.com>
2024-04-19 14:47:19 -0400

    print model

--- torchrun_main.py
+++ torchrun_main.py
@@ -398,6 +398,40 @@ def main(args):
     # we'll never go through all the data, so no need for epochs
     # ##############################
 
+    ##################### Added by NS ##################################################
+    # Log norms of weight matrices for linear projections
+    print(model)
+    _MODULE = model.module.wrapped_model.model if args.use_peft else model.module.model
+
+    # # Iterate over each parameter in the model
+    # for name, param in _MODULE.named_parameters():
+    #     # Print the parameter's name and whether it requires gradients
+    #     print(f"Parameter: {name}, Requires Grad: {param.requires_grad}")
+        
+    layer6_q_W0 = _MODULE.layers[6].self_attn.q_proj.weight.clone()
+    layer6_v_W0 = _MODULE.layers[6].self_attn.v_proj.weight.clone()
+    layer6_o_W0 = _MODULE.layers[6].self_attn.o_proj.weight.clone()
+    layer6_k_W0 = _MODULE.layers[6].self_attn.k_proj.weight.clone()
+
+    if args.use_peft:
+        q_Wa_0 = _MODULE.layers[6].self_attn.q_proj.lora_A.weight.clone()
+        q_Wb_0 = _MODULE.layers[6].self_attn.q_proj.lora_B.weight.clone()
+        q_WaWb_0 = q_Wa_0.T @ q_Wb_0.T
+
+        k_Wa_0 = _MODULE.layers[6].self_attn.k_proj.lora_A.weight.clone()
+        k_Wb_0 = _MODULE.layers[6].self_attn.k_proj.lora_B.weight.clone()
+        k_WaWb_0 = k_Wa_0.T @ k_Wb_0.T
+
+        v_Wa_0 = _MODULE.layers[6].self_attn.v_proj.lora_A.weight.clone()
+        v_Wb_0 = _MODULE.layers[6].self_attn.v_proj.lora_B.weight.clone()
+        v_WaWb_0 = v_Wa_0.T @ v_Wb_0.T
+
+        o_Wa_0 = _MODULE.layers[6].self_attn.o_proj.lora_A.weight.clone()
+        o_Wb_0 = _MODULE.layers[6].self_attn.o_proj.lora_B.weight.clone()
+        o_WaWb_0 = o_Wa_0.T @ o_Wb_0.T
+                
+    ####################################################################################
+
     for batch_idx, batch in enumerate(dataloader):
 
         global_step += 1
